cf22eaabb9fc9ec5c5e3991708715b6d3c421308
mazucanti <daniel.mazucanti.ismart@gmail.com>
2021-01-27 11:14:00 -0300

    Correção de url da api e remoção de funções de funcionalidade questionável

--- main.py
+++ main.py
@@ -1,51 +1,19 @@
+import calendar
 import datetime as dt
 from pathlib import Path
-import calendar
+from hashlib import sha256
+
 import numpy as np
+
 from src import functionsProspecAPI as prospec
 from src.arrFiles import main as prep_files
 
 
-def main():
-    prospec.authenticateProspec(
-        "daniel.mazucanti@skoposenergia.com.br", "Skopos2020")
-
-    numRequests = prospec.getNumberOfRequests()
-
-    print("Foram feitas %s requisições até o momento." % numRequests)
+def prep_n_run():
+    name, path = model_params()
 
     idServer = prospec.getIdOfServer("c5.24xlarge")
     idQueue = prospec.getIdOfFirstQueueOfServer("c5.24xlarge")
-
-    while True:
-        control_flow = input(
-            '1- Criar estudo\n2- Rodar estudo\n3- Abortar execução\n4- Informações do estudo\n0- Sair\n')
-        if control_flow == "1":
-
-            nameStudy, path_opt = model_params()
-
-            create_study(nameStudy)
-
-        elif control_flow == "2":
-
-            prep_run()
-            prospec.runExecution(studyId, idServer, idQueue, '', '0', '0', '2')
-
-        elif control_flow == "3":
-            display_studies()
-            stopId = int(input("Qual estudo deseja parar?\n"))
-            prospec.abortExecution(stopId)
-
-        elif control_flow == "4":
-            studiesInfo()
-
-        else:
-            print("Programa encerrado.")
-            break
-
-
-def prep_run():
-    name, path = model_params()
 
     display_studies()
 
@@ -65,21 +33,28 @@ def prep_run():
         duration = str(todayDate.month - targetDates[1].month)
         month = [targetDates[0].month, targetDates[1].month]
         year = [targetDates[0].year, targetDates[1].year]
-        dateToFormat = (initialYear, initialMonth)
-        newaveFile = "NW%d%d" % dateToFormat
-        decompFile = "DC%d%d" % dateToFormat
+        monthStr = '0' + \
+            str(initialMonth) if initialMonth < 10 else str(initialMonth)
+        dateToFormat = (initialYear, monthStr)
+        newaveFile = "NW%d%s.zip" % dateToFormat
+        decompFile = "DC%d%s.zip" % dateToFormat
         configFile = "Dados_Prospectivo.xlsx"
 
-    prospec.sendFileToStudy(studyId, path+'/'+configFile, configFile)
 
-    prospec.generateStudyDecks(
-        studyId, [initialYear], [initialMonth], [duration], month, year, [False, False], [True, True], newaveFile, "", decompFile, configFile, [])
+    prospec.sendFileToStudy(studyId,path+'/'+configFile, configFile)
 
     path_prevs = path + "/prevs/"
 
     prospec.sendPrevsToStudy(studyId, path_prevs)
 
     send_gevazp(path, studyId)
+
+    prospec.runExecution(studyId, idServer, idQueue, '', '0', '0', '2')
+    prospec.generateNextRev(studyId, newaveFile, decompFile, configFile, [])
+
+    # prospec.generateStudyDecks(studyId, initialYear, initialMonth, [duration], month, year, [False, False], [
+    #                            True, True], newaveFile, [newaveFile, newaveFile], [decompFile, decompFile], [configFile, configFile], [])
+
 
 
 def treat_dates():
@@ -102,7 +77,7 @@ def get_rev(date_value):
     month = date_value.month
     year = date_value.year
     x = np.array(calendar.monthcalendar(year, month))
-    revision = np.where(x==day)[0][0]
+    revision = np.where(x == day)[0][0]
     return(revision)
 
 
@@ -157,13 +132,47 @@ def model_params():
         path = "CP/Curtissimo"
 
     elif choice < 4:
-        
+
         print("Opção em implementação.")
 
     else:
         print("Opção inválida.")
 
     return nameStudy, path
+
+
+def main():
+    prospec.authenticateProspec(
+        "daniel.mazucanti@skoposenergia.com.br", "Skopos2020")
+
+    numRequests = prospec.getNumberOfRequests()
+
+    print("Foram feitas %s requisições até o momento." % numRequests)
+
+    while True:
+        control_flow = input(
+            '1- Criar estudo\n2- Rodar estudo\n3- Abortar execução\n4- Informações do estudo\n0- Sair\n')
+        if control_flow == "1":
+
+            nameStudy, path_opt = model_params()
+
+            create_study(nameStudy)
+
+        elif control_flow == "2":
+
+            prep_n_run()
+
+        elif control_flow == "3":
+            display_studies()
+            stopId = int(input("Qual estudo deseja parar?\n"))
+            prospec.abortExecution(stopId)
+
+        elif control_flow == "4":
+            studiesInfo()
+
+        else:
+            print("Programa encerrado.")
+            break
 
 
 main()
